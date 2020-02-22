67b48a2e40ae65068fed86bb138219407f5384de
Asier Garcia Ruiz <asier.garciaruiz@gmail.com>
2020-02-22 08:13:43 -0500

    Takes in all csv files in dir and merges them.

--- dataAnalysis.R
+++ dataAnalysis.R
@@ -1,4 +1,31 @@
-# This is the file to modify the dataset
-# We are going to merge the csv files into a big one first
+#!/bin/R
+
+# GT 4801 Open Data with R Project
+# Author: Asier Garcia Ruiz
+
+# Package Installs
 # install.package(tidyverse)
-# install
+
+# Libraries needed
+library(readr)
+
+# Import all the (clean) data from ../cleanData
+# // TODO
+
+# Access all the files in the directory
+path = "~/gtech/gt4801R/cleanData/"
+outFile <- ""
+fileNames <- dir(path, pattern=".csv")
+
+# Read in the first file
+joinedData <- read_csv(paste(path, fileNames[1], sep=""))
+
+# All the files have the same variables
+# We save the header row in a variable (grabbed from 1st dataset)
+headerRow <- head(joinedData, 1)
+
+# Loop through all the files and join them
+for (i in 2:length(fileNames)) {
+  file <- read_csv(paste(path, fileNames[i], sep=""))
+  joinedData <- rbind(tail(file, -1), joinedData)
+}
