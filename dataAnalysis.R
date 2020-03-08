1b119fe707c41606215cb0aea485216a9794e64f
Asier Garcia Ruiz <asier.garciaruiz@gmail.com>
2020-03-08 18:03:39 -0400

    Nearly have the dataframe I need

--- dataAnalysis.R
+++ dataAnalysis.R
@@ -4,15 +4,17 @@
 # Author: Asier Garcia Ruiz
 
 # Package Installs
-# install.package(tidyverse)
-# install.package(skimr)
-# install.package(ggplot2)
+# install.packages("tidyverse")
+# install.packages("skimr")
+# install.packages("ggplot2")
+# install.packages("astsa")
 
 # Libraries needed
 library(tidyverse)
 library(skimr)
 library(readr)
 library(ggplot2)
+library(astsa)
 
 # Import all the (clean) data from ../cleanData
 # // TODO
@@ -45,11 +47,32 @@ summary(data)
 skim(data)
 unique(data)
 
-# First column is indexing so we can delete
-cleanData$X <- NULL
-# Second column is always the same so we can delete too
-cleanData$X0 <- NULL
+tibbleData <- as_tibble(data)
 
 # Select interesting columns
-timeAnalysis <- data[, c()]
+interestData <- tibbleData[, c("descripcion_cliente",
+                             "numero_factura",
+                             "fecha_hora_factura",
+                             "descripcion_producto_cliente",
+                             "precio_base_producto",
+                             "cantidad",
+                             "numero_comensales")]
+
+# Let's start one of out analyses
+# We're going to look at the number of sales in total
+# Since the data seems pretty heterogeneous we will not separate by client
+
+salesByNum <- interestData[, c("numero_factura", 
+                              "fecha_hora_factura")]
+# Since there is more than one item per bill lets grab only the different ones
+# We use the fact that they all should have the same establishment and purchase time
+salesByNum <- distinct(salesByNum)
+# Interesting fact, the number of rows got divided by 3
+# Convert column into datetime objects
+salesByNum$fecha_hora_factura <- as.Date(as.character(salesByNum$fecha_hora_factura,
+                                                      format = "%d/%m/%Y"))
+# The year parses wrong but it is all 2019 so we can just remove it
+salesByNum$fecha_hora_factura <- format(salesByNum$fecha_hora_factura, format = "%Y/%m")
+# Sort by date (date is in form DD/MM/YYYY)
+salesByNum <- salesByNum[order(salesByNum$fecha_hora_factura),]
 
